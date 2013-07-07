1c412d7dfa023a8fe9d2f304599247a9b5f6005e
Ubuntu <ubuntu@ip-10-203-7-37.ec2.internal>
2013-07-07 13:30:32 +0000

    Fix bugs in grader.js

--- grader.js
+++ grader.js
@@ -24,10 +24,10 @@ References
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html");
+var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-var assertsFileExists = function(infile) {
+var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
@@ -36,7 +36,15 @@ var assertsFileExists = function(infile) {
     return instr;
 };
 
-var checkHtmlFile = function(htmlFile, checksfile) {
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
